b2e5dd453d304a96c38db59e67591ac1e603c3ea
axelmitschidev <axel.mitschi@gmail.com>
2023-02-25 18:59:03 +0100

    initialisation du server

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 require('dotenv').config()
+const express = require('express')
+const path = require('path')
+const app = express()
 
-console.log(process.env.API_KEY)
+const PORT = process.env.SERVER_PORT
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+})
+
+app.listen(PORT, () => console.log(`Listen on port ${PORT}`))
