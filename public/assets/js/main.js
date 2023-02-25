63b60d36a1daa22c5f2d3cc67989a0280f6f5ed2
Bontano <hassen.tom07@gmail.com>
2023-02-25 19:49:09 +0100

    mod

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -2,7 +2,9 @@ const socket = io()
 
 const btn_send_msg_element = document.getElementById("btn-send-msg")
 
+const textarea_msg_element = document.getElementById("textarea-msg")
+
 btn_send_msg_element.addEventListener("click", (e) => {
     e.preventDefault()
-    socket.emit('chat message', "pong")
+    socket.emit('chat message', textarea_msg_element.value)
 })
