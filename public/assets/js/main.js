150fbce278dc3102fb9c98a46396874c358f8139
Bontano <hassen.tom07@gmail.com>
2023-02-25 19:42:15 +0100

    ozeizoi

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,1 +1,8 @@
 const socket = io()
+
+const btn_send_msg_element = document.getElementById("btn-send-msg")
+
+btn_send_msg_element.addEventListener("click", (e) => {
+    e.preventDefault()
+    socket.emit('chat message', "pong")
+})
