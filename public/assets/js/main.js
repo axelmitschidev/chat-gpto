b823fb2c06bf5625a4613a33e8e573af5f7f6fcc
Bontano <hassen.tom07@gmail.com>
2023-02-28 00:25:18 +0100

    input vider

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,12 +1,13 @@
 const socket = io()
 
 const btn_send_msg_element = document.getElementById("btn-send-msg")
-const textarea_msg_element = document.getElementById("textarea-msg")
+const input_msg_element = document.getElementById("input-msg")
 const body_element = document.getElementsByTagName("body")[0]
 
 btn_send_msg_element.addEventListener("click", (e) => {
     e.preventDefault()
-    socket.emit('chat message', textarea_msg_element.value)
+    socket.emit('chat message', input_msg_element.value)
+    input_msg_element.value=""
 })
 
 socket.on("draw message", (msg) => {
