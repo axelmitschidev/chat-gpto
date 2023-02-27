6a7a6590d888a3ad6f5323f0c0ea20740c3e83c1
Bontano <hassen.tom07@gmail.com>
2023-02-28 00:53:18 +0100

    input vider2

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const SocketAntiSpam  = require('socket-anti-spam')
+
 require('dotenv').config()
 const path = require('path')
 
@@ -10,6 +12,18 @@ const { Server } = require('socket.io')
 const io = new Server(server)
 
 const PORT = process.env.SERVER_PORT
+
+const socketAntiSpam = new SocketAntiSpam({
+    banTime:            30,         // Ban time in minutes
+    kickThreshold:      2,          // User gets kicked after this many spam score
+    kickTimesBeforeBan: 1,          // User gets banned after this many kicks
+    banning:            true,       // Uses temp IP banning after kickTimesBeforeBan
+    io:                 socket-io,  // Bind the socket.io variable
+    redis:              client,      // Redis client if you are sharing multiple servers
+  })
+
+
+
 
 app.use('/assets', express.static(path.join(__dirname, './public/assets/')))
 
