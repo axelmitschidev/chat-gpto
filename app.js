0bfb3194d4f0ac02dc0d10bfeeceb195ee3f1b1e
axelmitschidev <axel.mitschi@gmail.com>
2023-02-28 01:31:45 +0100

    .

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-const SocketAntiSpam  = require('socket-anti-spam')
-
 require('dotenv').config()
 const path = require('path')
 
@@ -11,33 +9,60 @@ const server = http.createServer(app)
 const { Server } = require('socket.io')
 const io = new Server(server)
 
+const banned_ips = []
+const history_ips = []
+let admin_ip = null
+
 const PORT = process.env.SERVER_PORT
-
-const socketAntiSpam = new SocketAntiSpam({
-    banTime:            30,         // Ban time in minutes
-    kickThreshold:      2,          // User gets kicked after this many spam score
-    kickTimesBeforeBan: 1,          // User gets banned after this many kicks
-    banning:            true,       // Uses temp IP banning after kickTimesBeforeBan
-    io:                 socket-io,  // Bind the socket.io variable
-    redis:              client,      // Redis client if you are sharing multiple servers
-  })
-
-
-
 
 app.use('/assets', express.static(path.join(__dirname, './public/assets/')))
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'))
+  res.sendFile(path.join(__dirname, './public/index.html'))
 })
 
-io.on('connection', socket => {
-    console.log('A user is connected')
+io.on('connection', (socket) => {
+  if (!history_ips.includes(socket.handshake.address)) {
+    history_ips.push(socket.handshake.address)
+  }
 
-    socket.on('chat message', msg => {
-        socket.emit('draw message', msg)
-        socket.broadcast.emit('draw message', msg)
-    })
+  socket.on('chat message', (msg) => {
+    if (msg.length <= 256 && !banned_ips.includes(socket.handshake.address)) {
+      socket.emit('draw message', msg)
+      socket.broadcast.emit('draw message', msg)
+    }
+  })
+
+  socket.on('admin connect', (data) => {
+    if (
+      data.username === process.env.ADMIN_USERNAME &&
+      data.password === process.env.ADMIN_PASSWORD
+    ) {
+      socket.emit('admin connect', { history_ips, banned_ips })
+      admin_ip = socket.handshake.address
+    }
+  })
+
+  socket.on('ban ip', (ip) => {
+    if (socket.handshake.address === admin_ip) {
+      banned_ips.push(ip)
+    }
+  })
+
+  socket.on('unban ip', (ip) => {
+    if (socket.handshake.address === admin_ip) {
+      const index = banned_ips.findIndex((banned_ip) => banned_ip === ip)
+      if (index !== -1) {
+        banned_ips.splice(index, 1)
+      }
+    }
+  })
+
+  socket.on('show ips', () => {
+    if (socket.handshake.address === admin_ip) {
+      socket.emit('admin connect', { history_ips, banned_ips })
+    }
+  })
 })
 
 server.listen(PORT, () => console.log(`Listen on port ${PORT}`))
--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,17 +1,40 @@
 const socket = io()
 
-const btn_send_msg_element = document.getElementById("btn-send-msg")
-const input_msg_element = document.getElementById("input-msg")
-const body_element = document.getElementsByTagName("body")[0]
+const btn_send_msg_element = document.getElementById('btn-send-msg')
+const input_msg_element = document.getElementById('input-msg')
+const body_element = document.getElementsByTagName('body')[0]
 
-btn_send_msg_element.addEventListener("click", (e) => {
-    e.preventDefault()
-    socket.emit('chat message', input_msg_element.value)
-    input_msg_element.value=""
+btn_send_msg_element.addEventListener('click', (e) => {
+  e.preventDefault()
+  socket.emit('chat message', input_msg_element.value)
+  input_msg_element.value = ''
 })
 
-socket.on("draw message", (msg) => {
-    const p_element = document.createElement('p')
-    p_element.textContent = msg
-    body_element.appendChild(p_element)
+socket.on('draw message', (msg) => {
+  const p_element = document.createElement('p')
+  p_element.textContent = msg
+  body_element.appendChild(p_element)
 })
+
+socket.on('admin connect', (data) => {
+  console.log(data)
+})
+
+function admin_connect(username, password) {
+  socket.emit('admin connect', {
+    username,
+    password,
+  })
+}
+
+function ban_ip(ip) {
+  socket.emit('ban ip', ip)
+}
+
+function unban_ip(ip) {
+  socket.emit('unban ip', ip)
+}
+
+function show_ips() {
+  socket.emit('show ips')
+}
